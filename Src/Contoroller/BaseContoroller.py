cb2a6111b8f6f77b3fcdbf8c146fa057127dd112
Mohamed Ehab <ma0587580@gmail.com>
2025-12-04 08:49:53 +0200

    Add BaseContoroller and VectorDBProviderFactory for database path management and provider creation

--- Src/Contoroller/BaseContoroller.py
+++ Src/Contoroller/BaseContoroller.py
@@ -9,5 +9,18 @@ class BaseContoroller:
         self.base_dir= os.path.dirname(os.path.dirname(__file__))
         self.file_dirs=os.path.join (self.base_dir,'assets/Files')
 
+        self.database_dir = os.path.join(
+            self.base_dir,
+            "assets/database"
+        )
+
     def generate_random_string(self, length: int=12):
              return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    
+    def get_db_path(self,provider_name:str):
+          path_db=os.path.join(self.database_dir,provider_name)
+
+          if not os.path.exists(path_db):
+                os.makedirs(path_db)
+          
+          return path_db
